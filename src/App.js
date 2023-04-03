6d27e1dd3818e59d75e767f60d3db0f3827977fc
IbadAhmad80 <ibad23ahmda@gmail.com>
2023-04-04 04:34:45 +0500

    Setup project boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+   return (
+      <div className="App">
+         <h1 style={heading}>This is the starting of The Outreach :)</h1>
+      </div>
+   );
+}
+
+const heading = {
+   textAlign: "center",
+   transform: "translate(0,45vh)",
+};
+
+export default App;
